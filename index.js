54489c55f5289f789e15d3ba4c9607f5f5c904eb
manymusa <adanalin113@gmail.com>
2022-08-29 17:00:05 -0400

    commit tree 9be8ea5dffc99491f1831a1415c4452d442821bc

--- index.js
+++ index.js
@@ -9,6 +9,54 @@
  for you to use if you need it!
  */
 
+ function createEmployeeRecord(employeeArr) {
+    return {
+        'firstName': employeeArr[0],
+        'familyName': employeeArr[1],
+        'title': employeeArr[2],
+        'payPerHour': employeeArr[3],
+        'timeInEvents': [],
+        'timeOutEvents': []
+    }
+}
+
+function createEmployeeRecords(employeesArr){
+    return employeesArr.map(employeeArr => createEmployeeRecord(employeeArr))
+}
+
+function createTimeInEvent(dateStamp){
+    const dateAndTime = dateStamp.split(' ');
+    this.timeInEvents.push({
+        'type': 'TimeIn',
+        'hour': parseInt(dateAndTime[1], 10),
+        'date': dateAndTime[0]
+    })
+    return this
+}
+
+function createTimeOutEvent(dateStamp){
+    const dateAndTime = dateStamp.split(' ');
+    this.timeOutEvents.push ({
+        'type': 'TimeOut',
+        'hour': parseInt(dateAndTime[1], 10),
+        'date': dateAndTime[0]
+    })
+    return this
+}
+
+function hoursWorkedOnDate(date){
+    for(let i = 0; i < this.timeInEvents.length; i++){
+        if(this.timeInEvents[i].date === date && this.timeInEvents[i].date){
+            return (this.timeOutEvents[i].hour - this.timeInEvents[i].hour)/100
+        }
+    }
+}
+
+function wagesEarnedOnDate(date){
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+
+
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -21,3 +69,17 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName (srcArray,firstName){
+    let match = undefined;
+    srcArray.find(employee => {
+        if(employee.firstName === firstName){
+            match = employee;
+         }
+    })
+    return match
+}
+
+function calculatePayroll(employessArr){
+    let wagesArr = employessArr.map(employee => allWagesFor.call(employee))
+    return wagesArr.reduce((total,wage) => total += wage,0)
+}
